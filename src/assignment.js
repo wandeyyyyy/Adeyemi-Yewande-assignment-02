d2851e6b1b84648d441c673f4ba0beac7ad03a1f
Yewande Adeyemi <yewandeadeyemi84@gmail.com>
2022-07-11 21:36:58 +0000

    check challenge - 1 check challenge - 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,21 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+function sumOfNumbers(aSimpleArray) {
+
+  
+  let sumOfArrays = 0;
+  for (let i = 0; i < aSimpleArray.length; i++) {
+    sumOfArrays += aSimpleArray[i];
+    
+  }
+  return(sumOfArrays);
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+
+
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,10 +39,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++){
+    
+if (arrayOfNumbers[i] % 2 == 0 ) {
+    count++;
+  }
+  }
+  return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
-
+assignment.countEvenNumbers = countEvenNumbers; 
+ 
 /**
  * Challenge - 3
  * 
